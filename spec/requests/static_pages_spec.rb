da14620a908dd85f34bb9f7a6bb5e81c7fa3f540
Ty Richards <me@tyrichards.com>
2014-04-29 15:43:48 -0500

    setup Guard & Spork testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,10 +4,11 @@ describe "Static pages" do
     let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
     describe "Home page" do
+
         it "should have the content 'Sample App'" do
           visit '/static_pages/home'
           expect(page).to have_content('Sample App')
-        end
+        end            
 
         it "should have the title 'Home'" do
             visit '/static_pages/home'
