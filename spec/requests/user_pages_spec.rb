e6a4e8d7fd5cb955d406266cc626addfa0d20ef9
Ty Richards <me@tyrichards.com>
2014-05-06 10:41:36 -0500

    Add User Resource & Factory Girl Gem

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,8 +6,14 @@ describe "UserPages" do
     
     describe "Signup page" do
         before { visit signup_path }
-
         it { should have_content('Sign Up') }
         it { should have_title(full_title('Sign Up')) }
+    end
+
+    describe "Profile page" do
+        # Replace with code to make a user variable
+        before { visit user_path(user) }
+        it { should have_content(user.name) }
+        it { should have_title(user.name) }
     end
 end
