123e98868cadc5b474b2e20b12f8c73c141defba
Ty Richards <me@tyrichards.com>
2014-05-17 14:37:19 -0500

    Ch 9 Exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-    before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
-    before_action :correct_user,   only: [:edit, :update]
-    before_action :admin_user,     only: :destroy
+    before_action :signed_in_user,      only: [:index, :edit, :update, :destroy]
+    before_action :correct_user,        only: [:edit, :update]
+    before_action :admin_user,          only: :destroy
+    before_action :signed_in_user_new,  only: [:new, :create]
 
     def show  
         @user = User.find(params[:id])
@@ -36,11 +37,16 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
-    def destroy
-        User.find(params[:id]).destroy
-        flash[:success] = "User deleted."
-        redirect_to users_url
+def destroy
+    user = User.find(params[:id])
+    if (current_user == user) && (current_user.admin?)
+      flash[:error] = "Can not delete own admin account!"
+    else
+      user.destroy
+      flash[:success] = "User destroyed."
     end
+  redirect_to users_path
+  end
 
     def index
         @users = User.paginate(page: params[:page])
@@ -54,11 +60,15 @@ class UsersController < ApplicationController
 
         # Before filters
 
-        def signed_in_user
+        def signed_in_user 
             unless signed_in?
                 store_location
                 redirect_to signin_url, notice: "Please sign in."
-            end            
+            end          
+        end
+
+        def signed_in_user_new
+            redirect_to root_url, notice: "You are already logged in" if signed_in?
         end
 
         def correct_user
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -38,29 +38,58 @@ describe "Authentication" do
 
             describe "followed by signout" do
                 before { click_link "Sign out" }
-                it { should have_link('Sign in') }
+                it { should have_link('Sign in', href: signin_path) }
+                
+                it { should_not have_link('Users',       href: users_path) }
+                it { should_not have_link('Profile',     href: user_path(user)) }
+                it { should_not have_link('Settings',    href: edit_user_path(user)) }
+                it { should_not have_link('Sign out',    href: signout_path) }               
             end            
         end
     end    
 
     describe "authorization"  do
 
+        describe "for signed-in users" do
+            let(:user) { FactoryGirl.create(:user) }
+            let(:new_user) { FactoryGirl.create(:user) }
+            before { sign_in user, no_capybara: true }
+
+            describe "using a 'new' action" do
+                before { get new_user_path }
+                specify { expect(response).to redirect_to(root_url) }
+            end
+
+            describe "using a 'create' action" do
+                before { post users_path(new_user) }
+                specify { expect(response).to redirect_to(root_url) }
+            end            
+        end
+
         describe "for non-signed-in users" do
             let(:user) { FactoryGirl.create(:user) }
 
             describe "when attempting to visit a protected page" do
                 before do
-                  visit edit_user_path(user)
-                  fill_in "Email",    with: user.email
-                  fill_in "Password", with: user.password
-                  click_button "Sign in"
+                    visit edit_user_path(user)
+                    sign_in user
                 end
 
                 describe "after signing in" do
+                    it "should render the desired protected page" do
+                        expect(page).to have_title('Edit user')
+                    end
 
-                  it "should render the desired protected page" do
-                    expect(page).to have_title('Edit user')
-                  end
+                    describe "when signing in again" do
+                        before do
+                            click_link "Sign out"
+                            sign_in user
+                        end
+
+                        it "should render the default (profile) page" do
+                            expect(page).to have_title(user.name)
+                        end
+                    end
                 end
             end
 
@@ -108,6 +137,15 @@ describe "Authentication" do
             describe "submitting a DELETE request to the Users#destroy action" do
                 before { delete user_path(user) }
                 specify { expect(response).to redirect_to(root_url) }
+            end
+        end
+
+        describe "as admin user" do
+            let(:admin) { FactoryGirl.create(:admin) }
+            before { sign_in admin }
+
+            it "should not be able to delete itself" do
+                expect { delete user_path(admin) }.not_to change(User, :count)
             end
         end
     end
