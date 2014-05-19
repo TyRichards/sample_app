96831d9839e4326fd30a152eac67708df090fbc0
Ty Richards <me@tyrichards.com>
2014-05-19 16:33:36 -0500

    add follow methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@ describe User do
 
     subject { @user }
 
+    # works with @user.name or @user.email etc etc
     it { should respond_to(:name) }
     it { should respond_to(:email) }
     it { should respond_to(:password_digest) }
@@ -20,6 +21,10 @@ describe User do
     it { should respond_to(:microposts) }
     it { should respond_to(:feed) }
     it { should respond_to(:relationships) }
+    it { should respond_to(:followed_users) }
+    it { should respond_to(:following?) }
+    it { should respond_to(:follow!) }
+    it { should respond_to(:unfollow!) }
 
     it { should be_valid }
     it { should_not be_admin }
@@ -160,4 +165,22 @@ describe User do
         before { @user.save }
         its(:remember_token) { should_not be_blank }
     end 
+
+    describe "following" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+            @user.save
+            @user.follow!(other_user)
+        end
+
+        it { should be_following(other_user) }
+        its(:followed_users) { should include(other_user) }
+
+        describe "and unfollowing" do
+            before { @user.unfollow!(other_user) }
+
+            it { should_not be_following(other_user) }
+            its(:followed_users) { should_not include(other_user) }
+        end
+    end
 end
